ffc8f6677063598a0ce563073d3f15f2215110c9
Iyappan-13 <iyappan16052005@gmail.com>
2025-03-09 12:25:22 +0530

    personal-api.py

--- personal-api.py
+++ personal-api.py
@@ -1,4 +1,19 @@
-Update this file to add your code
-/name - Should give your name
-/register_number - Should give your register_number
-/department - Should give your department
+from flask import Flask
+
+app =Flask(_name_)
+
+@app.route("/")
+def name():
+    return "iyappan "
+
+
+@app.route("/registerNumber")
+def register_number():
+    return "22IT013"
+
+@app.route("/department")
+def department():
+    return "IT(Information Technology)"
+
+if _name_ == "_main_":
+    app.run(debug=True)
